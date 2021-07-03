51e867c7134bcb43ebfc904ba2c6809e2e9a33d0
marymount-writers <86791436+marymount-writers@users.noreply.github.com>
2021-07-03 12:32:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,10 @@ import math
 import pandas as pd
 import streamlit as st
 
+def get_session_state(rando):
+    session_state = SessionState.get(random_number=random.random(), nsamples='')
+    return session_state
+
 def display_app_header(main_txt,sub_txt,is_sidebar = False):
     html_temp = f"""
     <h2 style = "color:black; text_align:center;"> {main_txt} </h2>
@@ -14,13 +18,19 @@ def display_app_header(main_txt,sub_txt,is_sidebar = False):
         st.sidebar.markdown(html_temp, unsafe_allow_html = True)
     else: 
         st.markdown(html_temp, unsafe_allow_html = True)
+        
+def display_side_panel_header(txt):
+    st.sidebar.markdown(f'## {txt}')
 
 def main():
     st.set_page_config(page_title='Calibre') #layout='wide', initial_sidebar_state='auto'
+    v_nsamples = int(os.getenv('V_NSAMPLES', 3)) # Number of competitors to generate
     sep = '<|endoftext|>'
     main_txt = """Calibre"""
     sub_txt = "Competitor Analysis"
     display_app_header(main_txt,sub_txt,is_sidebar = False)
+    display_side_panel_header("Configuration")
+    session_state.nsamples = st.sidebar.slider("Number of Competitors to Analyse: ", 1, v_nsamples, 1)
     
 if __name__ == "__main__":
     main()
