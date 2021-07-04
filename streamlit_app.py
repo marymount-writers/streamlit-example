f9231566314479c9eef8c615fefd390181a06d73
marymount-writers <86791436+marymount-writers@users.noreply.github.com>
2021-07-04 14:54:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,6 +83,7 @@ def main():
         if st.button('Generate Competitor Analysis'):
             session_state.analysis_running = True
             session_state.generated = generate_competitors(session_state.domain,session_state.industry,session_state.nsamples)
+            competitors_df = session_state.generated
 #             if st.button(label='Reset'):
 #                 session_state.analysis_running = False
 #                 st.experimental_rerun()
@@ -92,8 +93,7 @@ def main():
             st.dataframe(session_state.generated)
             with st.form(key='content_brief'):
                 competitors_selected = st.multiselect(label="Choose the competitor(s) for content brief generation: ", 
-                                                     options=session_state.generated.iloc[:,0],
-                                                     default=session_state.generated.iloc[0,0])
+                                                     options=competitors_df.iloc[:,0])
                 submit_competitors = st.form_submit_button(label='Save Competitors')
                 if submit_competitors:
                     st.write('You have saved: {}'.format(competitors_selected))
