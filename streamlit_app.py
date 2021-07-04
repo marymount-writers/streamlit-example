d2b2d8eb9aa558f8c3e4ddc0c1d7270cf12389a9
marymount-writers <86791436+marymount-writers@users.noreply.github.com>
2021-07-04 14:40:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,17 +70,15 @@ def main():
     
     ### USER PROFILE ###
     if session_state.pages == 'User Profile':
-        with st.form(key='tone_profile'):
-            st.markdown(f"""Your Preferred Writing Style""")
-            styles = st.multiselect(label='Select the writing style(s) you would like to adopt: ', options=writing_styles)
-            submit_button = st.form_submit_button(label='Submit')
+#         with st.form(key='tone_profile'):
+#             st.markdown(f"""Your Preferred Writing Style""")
+#             styles = st.multiselect(label='Select the writing style(s) you would like to adopt: ', options=writing_styles)
+#             submit_button = st.form_submit_button(label='Submit')
 
         session_state.domain = st.text_input("Your Website Domain: ", value='https://marymountwriters.com').lower()
         session_state.industry = st.text_input("Your Industry : ", value='Digital content marketing').lower()
     
     ### GENERATE COMPETITORS ###
-    
-    ### GENERATE CONTENT BRIEF ###
     if session_state.pages == 'Generate Competitors':
         if st.button('Generate Competitor Analysis'):
             session_state.analysis_running = True
@@ -89,18 +87,18 @@ def main():
         if session_state.analysis_running:
             st.header('Your competitors:')
             st.dataframe(session_state.generated)
-
             with st.form(key='content_brief'):
                 competitors_selected = st.multiselect(label="Choose the competitor(s) for content brief generation: ", options=session_state.generated.iloc[:,0])
-
                 if st.form_submit_button(label='Generate Content Brief'):
                     st.header('Your content brief:')
                     data = np.random.randn(10, 2)
                     chart = st.line_chart(data)
 
-            if st.button(label='Reset'):
-                session_state.analysis_running = False
-                st.experimental_rerun()
+    ### GENERATE CONTENT BRIEF ###
+    if session_state.pages == 'Generate Content Brief':
+#             if st.button(label='Reset'):
+#                 session_state.analysis_running = False
+#                 st.experimental_rerun()
        
 if __name__ == "__main__":
     main()
