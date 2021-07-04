10b95ef472450e0f9914379f94b8323598fdad8b
marymount-writers <86791436+marymount-writers@users.noreply.github.com>
2021-07-04 00:31:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,7 +81,7 @@ def main():
         
         with st.form(key='competitors_seleted'):
             competitors_selected = st.multiselect(label="Choose the competitor(s) for content brief generation: ", options=session_state.generated.iloc[:,0])
-            st.form_submit_button(label='Generate Content Brief')
+            generate_content_brief = st.form_submit_button(label='Generate Content Brief')
        
 if __name__ == "__main__":
     main()
