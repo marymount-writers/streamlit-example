bfeeb9acb5f3ac55166046dde1c21814236090cb
marymount-writers <86791436+marymount-writers@users.noreply.github.com>
2021-07-04 15:06:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,9 +96,10 @@ def main():
                 competitors_selected = st.multiselect(label="Choose the competitor(s) for content brief generation: ", 
                                                      options=session_state.generated.iloc[:,0])
                 submit_competitors = st.form_submit_button(label='Save Competitors')
-                if submit_competitors:
-                    st.write('You have saved: {}'.format(competitors_selected))
-                    session_state.analysis_running = False
+                while submit_competitors is False:
+                    if submit_competitors:
+                        st.write('You have saved: {}'.format(competitors_selected))
+                        session_state.analysis_running = False
                     
     ### GENERATE CONTENT BRIEF ###
             if session_state.pages == 'Generate Content Brief':
