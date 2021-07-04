47c309648a9cc2bb08593cb7b3499e61dd66d3f1
marymount-writers <86791436+marymount-writers@users.noreply.github.com>
2021-07-04 01:03:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,10 +84,10 @@ def main():
     if session_state.analysis_running:
         session_state.competitors_selected = st.multiselect(label="Choose the competitor(s) for content brief generation: ", options=session_state.generated.iloc[:,0])
 
-        if st.button(label='Generate Content Brief'):
-            st.header('Your content brief:')
-            data = np.random.randn(10, 2)
-            chart = st.line_chart(data)
+    if st.button(label='Generate Content Brief'):
+        st.header('Your content brief:')
+        data = np.random.randn(10, 2)
+        chart = st.line_chart(data)
        
 if __name__ == "__main__":
     main()
