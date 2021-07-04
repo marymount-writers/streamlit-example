1773ad0b981843da315ab041c6c5e88730ed613d
marymount-writers <86791436+marymount-writers@users.noreply.github.com>
2021-07-04 01:21:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,7 +94,7 @@ def main():
         if st.button(label='Reset'):
             session_state.analysis_running = False
             st.experimental_rerun()
-            break
+        break
        
 if __name__ == "__main__":
     main()
