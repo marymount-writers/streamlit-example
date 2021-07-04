7be7cb439d2c46f780ec1f0403f5a514499ac7c3
marymount-writers <86791436+marymount-writers@users.noreply.github.com>
2021-07-04 15:26:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,7 +92,7 @@ def main():
 #                 session_state.analysis_running = False
 #                 st.experimental_rerun()
 
-        if session_state.analysis_running:
+        if analysis_running:
             with st.form(key='content_brief'):
                 competitors_selected = st.multiselect(label="Choose the competitor(s) for content brief generation: ", 
                                                      options=session_state.generated.iloc[:,0])
