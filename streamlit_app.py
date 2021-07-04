837a4104ff4525a14785c34e4d51f760a78eae69
marymount-writers <86791436+marymount-writers@users.noreply.github.com>
2021-07-04 14:44:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,19 +83,15 @@ def main():
         if st.button('Generate Competitor Analysis'):
             session_state.analysis_running = True
             session_state.generated = generate_competitors(session_state.domain,session_state.industry,session_state.nsamples)
-            if st.button(label='Reset'):
-                session_state.analysis_running = False
-                st.experimental_rerun()
+#             if st.button(label='Reset'):
+#                 session_state.analysis_running = False
+#                 st.experimental_rerun()
 
         if session_state.analysis_running:
             st.header('Your competitors:')
             st.dataframe(session_state.generated)
             with st.form(key='content_brief'):
                 competitors_selected = st.multiselect(label="Choose the competitor(s) for content brief generation: ", options=session_state.generated.iloc[:,0])
-                if st.form_submit_button(label='Generate Content Brief'):
-                    st.header('Your content brief:')
-                    data = np.random.randn(10, 2)
-                    chart = st.line_chart(data)
 
     ### GENERATE CONTENT BRIEF ###
     if session_state.pages == 'Generate Content Brief':
