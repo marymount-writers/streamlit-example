d4106aa91a3fc1144851326d66e28fbef58b5063
marymount-writers <86791436+marymount-writers@users.noreply.github.com>
2021-07-04 18:07:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,11 +85,11 @@ def main():
     ### GENERATE COMPETITORS ###
 #     competitor_df = pd.DataFrame(columns=['Competitor','Similarity','Channels','Target Keywords'])
     if session_state.pages == 'Generate Competitors':
-#         if st.button('Generate Competitor Analysis'):
-        session_state.generated = generate_competitors(session_state.domain,session_state.industry,session_state.nsamples)
-        st.header('Your competitors:')
-        st.dataframe(session_state.generated)
-        competitors = session_state.generated.iloc[:,0]
+        if st.button('Generate Competitor Analysis'):
+            session_state.generated = generate_competitors(session_state.domain,session_state.industry,session_state.nsamples)
+            st.header('Your competitors:')
+            st.dataframe(session_state.generated)
+            competitors = session_state.generated.iloc[:,0]
 #         competitor_df = competitor_df.append(st.session_state.generated)
 
 #         with st.form(key='content_brief'):
