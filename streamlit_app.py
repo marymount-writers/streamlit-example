1d83a1b1f27cdb8aed921825f53dd1f3e4bad73a
marymount-writers <86791436+marymount-writers@users.noreply.github.com>
2021-07-04 16:03:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import os
 import SessionState
 
 def get_session_state(rando):
-    session_state = SessionState.get(random_number=random.random(), nsamples='', generate_competitors=pd.DataFrame())
+    session_state = SessionState.get(random_number=random.random(), nsamples='', generated=pd.DataFrame())
     return session_state
 
 def cacherando():
@@ -81,15 +81,17 @@ def main():
         session_state.industry = st.text_input("Your Industry : ", value='Digital content marketing').lower()
     
     ### GENERATE COMPETITORS ###
+    competitors = pd.DataFrame()
     if session_state.pages == 'Generate Competitors':
         if st.button('Generate Competitor Analysis'):
             session_state.generated = generate_competitors(session_state.domain,session_state.industry,session_state.nsamples)
+            competitors = session_state.generated
             st.header('Your competitors:')
             st.dataframe(session_state.generated)
 
         with st.form(key='content_brief'):
             competitors_selected = st.multiselect(label="Choose the competitor(s) for content brief generation: ", 
-                                                 options=session_state.generated.iloc[:,0])
+                                                 options=competitors.iloc[:,0])
             submit_competitors = st.form_submit_button(label='Save Competitors')
             if submit_competitors:
                 st.write('You have saved: {}'.format(competitors_selected))
