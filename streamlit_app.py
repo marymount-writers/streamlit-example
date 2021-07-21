8bb78de3f3dffa495e7faff4b8ebe8e26e6cd133
marymount-writers <86791436+marymount-writers@users.noreply.github.com>
2021-07-21 10:21:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,51 +55,38 @@ def main():
     display_app_header(main_txt,sub_txt,is_sidebar = False)
     awareness_stages = ['Unaware of solution','Aware of solution','Interested in product offered',
                         'Considering purchase','Intending to purchase','Existing product user']
-    writing_styles = ['As a leading product brand, JAVEN is a state-of-the-art rotator wing drone.',
-                     'As a leading drone brand, JAVEN is built on innovative rotator wing technology.',
-                     'The most innovative rotator wing technology underpins the success of JAVEN.',
-                     'With JAVEN rotator wings, you can lay back and watch the world burn.',
-                     'Innovative rotator wing technology is at the core of JAVEN.']
     
     ### SIDEBAR CONTENT ###
     display_side_panel_header("Menu")
-    session_state.pages = st.sidebar.radio("Navigate Calibre", options=['User Profile','Generate Competitors','Generate Content Brief'])
+    session_state.pages = st.sidebar.radio("Navigate Calibre", options=['Competitor Profile','Semantic Fingerprinting',
+                                                                        'Topical Matrix Analysis','Sentiment Heatmapping'])
     display_side_panel_header("Configuration")
     session_state.nsamples = st.sidebar.slider("Number of Competitors to Analyse: ", 1, v_nsamples, 1)
     display_side_panel_header("Audience Profile")
     session_state.audience_age = st.sidebar.slider("Audience Age Range: ", 16, 65, (26, 30))
     session_state.audience_awareness = st.sidebar.selectbox("Audience Awareness: ", options=awareness_stages)
     
-    ### USER PROFILE ###
-    if session_state.pages == 'User Profile':
-#         with st.form(key='tone_profile'):
-#             st.markdown(f"""Your Preferred Writing Style""")
-#             styles = st.multiselect(label='Select the writing style(s) you would like to adopt: ', options=writing_styles)
-#             submit_button = st.form_submit_button(label='Submit')
-
+    ### COMPETITOR PROFILE ###
+    if session_state.pages == 'Competitor Profile':
         session_state.domain = st.text_input("Your Website Domain: ", value='https://marymountwriters.com').lower()
         session_state.industry = st.text_input("Your Industry : ", value='Digital content marketing').lower()
-    
-    ### GENERATE COMPETITORS ###
-#     competitor_df = pd.DataFrame(columns=['Competitor','Similarity','Channels','Target Keywords'])
-    if session_state.pages == 'Generate Competitors':
-#         if st.button('Generate Competitor Analysis'):
+
         session_state.generated = generate_competitors(session_state.domain,session_state.industry,session_state.nsamples)
         st.header('Your competitors:')
         st.dataframe(session_state.generated)
-#         competitors = session_state.generated.iloc[:,0]
-#         competitor_df = competitor_df.append(st.session_state.generated)
+    
+    ### GENERATE COMPETITORS ###
+    if session_state.pages == 'Semantic Fingerprinting':
+        st.text("Semantic Fingerprinting")
 
-#         with st.form(key='content_brief'):
-        competitors_selected = st.multiselect(label="Choose the competitor(s) for content brief generation: ", 
-                                             options=session_state.generated.iloc[:,0])
-        submit_competitors = st.button(label='Save Competitors')
-        if submit_competitors:
-            st.write('You have saved: {}'.format(competitors_selected))
                     
     ### GENERATE CONTENT BRIEF ###
-    if session_state.pages == 'Generate Content Brief':
-        st.text("Content Brief")
+    if session_state.pages == 'Topical Matrix Analysis':
+        st.text("Topical Matrix Analysis")
+        
+    ### SENTIMENT HEATMAPPING ###
+    if session_state.pages == 'Sentiment Heatmapping':
+        st.text("Sentiment Heatmapping")
        
 if __name__ == "__main__":
     main()
